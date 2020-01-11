bb84dd68850da52fb65750efa707eb10f0059785
Minho Eom <djaalsgh159@kaist.ac.kr>
2020-01-11 23:33:59 +0900

    socket feature added

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,19 +1,36 @@
-const temp = document.querySelector("#temperature");
-const speed = document.querySelector("#speed");
-const button = document.querySelector("#set");
-const currentTmp = document.querySelector("#currentTmp");
+// const temp = document.querySelector("#temperature");
+// const speed = document.querySelector("#speed");
+// const button = document.querySelector("#set");
+// const currentTmp = document.querySelector("#currentTmp");
 
-let temp_value, speed_value;
+// let temp_value, speed_value;
 
-button.addEventListener("click", () => {
-  temp_value = temp.value;
-  speed_value = speed.value;
-  currentTmp.innerHTML = "clicked";
-  //PUT speed and temperature to API
+// button.addEventListener("click", () => {
+//   temp_value = temp.value;
+//   speed_value = speed.value;
+//   currentTmp.innerHTML = "clicked";
+//   //PUT speed and temperature to API
 
-  //fetch from the api and set innerHTML
-  setInterval(function() {
-    let time = Date.now();
-    currentTmp.innerHTML = time;
-  }, 500);
+//   //fetch from the api and set innerHTML
+//   setInterval(function() {
+//     let time = Date.now();
+//     currentTmp.innerHTML = time;
+//   }, 500);
+// });
+var socket = io();
+const form = document.querySelector("form");
+const message = document.querySelector("#m");
+const messages = document.querySelector("#messages");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  socket.emit("chat message", message.value);
+  message.value = "";
+  return false;
+});
+
+socket.on("chat message", msg => {
+  let node = document.createElement("li");
+  node.innerHTML = msg;
+  messages.append(node);
 });
