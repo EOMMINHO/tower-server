9f67b521edd37a24ad4d1f4a3923ecd42a412fb3
EOMMINHO <djaalsgh159@kaist.ac.kr>
2020-01-13 22:27:18 +0900

    stepper2 added

--- routes/api/stepper.js
+++ routes/api/stepper.js
@@ -4,21 +4,29 @@ const Readline = require("@serialport/parser-readline");
 var express = require("express");
 var router = express.Router();
 const stepper1 = new SerialPort(process.env.STEPPER1, { baudRate: 9600 });
+const stepper2 = new SerialPort(process.env.STEPPER2, { baudRate: 9600 });
 
 const parser1 = new Readline();
+const parser2 = new Readline();
 
 stepper1.pipe(parser1);
+stepper2.pipe(parser1);
 
 parser1.on("data", line => console.log(`> ${line}`));
+parser2.on("data", line => console.log(`> ${line}`));
 
 // current state of arduino
-let speed1 = 0;
-let direction1 = "+";
+let speed1 = 0,
+  speed2 = 0;
+let direction1 = "+",
+  direction2 = "+";
 
 router.get("/", function(req, res, next) {
   res.send({
     speed1: speed1,
-    direction1: direction1
+    speed2: speed2,
+    direction1: direction1,
+    direction2: direction2
   });
 });
 
@@ -26,10 +34,17 @@ router.get("/", function(req, res, next) {
 router.post("/", function(req, res, next) {
   speed1 = req.body.speed1;
   direction1 = req.body.direction1;
+  speed2 = req.body.speed2;
+  direction2 = req.body.direction2;
+  // command via serial
   stepper1.write(speed1 + direction1 + "\n");
+  stepper2.write(speed2 + direction2 + "\n");
+  // end command
   res.send({
     speed1: speed1,
-    direction1: direction1
+    speed2: speed2,
+    direction1: direction1,
+    direction2: direction2
   });
 });
 
