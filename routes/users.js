9a117fd9d3db82c445501152a0fd4c4a9d0d99a3
Minho Eom <djaalsgh159@kaist.ac.kr>
2020-01-20 02:05:55 +0900

    user db updated

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,36 @@
 require("dotenv").config();
+const { User } = require("../model/user");
+const mongoose = require("mongoose");
 const path = require("path");
 var debug = require("debug")("towerServer:server");
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.post("/signIn", function(req, res) {
+async function createUser(id, pw, isAdmin) {
+  //make user object
+  const user = new User({
+    ID: id,
+    PW: pw,
+    isAdmin: isAdmin
+  });
+
+  const result = await user.save();
+  debug(result._id);
+}
+
+async function getUsers(id) {
+  const users = await User.find({
+    ID: id
+  });
+  debug(users);
+}
+
+async function removeUser(id) {
+  const result = await User.deleteOne({ ID: id });
+  debug(result);
+}
+
+router.post("/signIn", async function(req, res) {
   let id = req.body.id;
   let pw = req.body.pw;
   debug(id);
@@ -19,6 +44,23 @@ router.post("/signIn", function(req, res) {
   } else {
     res.status(401).send("authentication wrong!");
   }
+});
+
+router.post("/signUp", async function(req, res) {
+  let id = req.body.id;
+  let pw = req.body.pw;
+  debug(id);
+  debug(pw);
+
+  const user = new User({
+    ID: id,
+    PW: pw,
+    isAdmin: false,
+    isAuthorized: false
+  });
+
+  const result = await user.save();
+  res.send(result._id);
 });
 
 module.exports = router;
