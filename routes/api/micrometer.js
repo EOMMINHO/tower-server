c25c3b6b50b6b988766d23dcf93209819bc9deb2
EOMMINHO <djaalsgh159@kaist.ac.kr>
2020-02-13 00:14:52 +0900

    diameter data check

--- routes/api/micrometer.js
+++ routes/api/micrometer.js
@@ -4,6 +4,7 @@ const SerialPort = require("serialport");
 const Readline = require("@serialport/parser-readline");
 var express = require("express");
 var router = express.Router();
+const Joi = require("@hapi/joi");
 
 // setting serial ports
 const micrometer = new SerialPort(process.env.MICROMETER_DEV, {
@@ -31,8 +32,23 @@ router.get("/", authUser, function(req, res, next) {
   });
 });
 
+// schema
+const schema = Joi.object({
+  diameter: Joi.number()
+    .integer()
+    .min(100)
+    .max(500)
+});
+
 router.post("/", authUser, function(req, res, next) {
   diameter = req.body.diameter;
+  // data type checking
+  const { error, value } = schema.validate({ diameter: diameter });
+  if (error !== undefined) {
+    res.send(value);
+  }
+
+  // if no error, send correct
   res.send("diameter set to " + diameter);
 });
 
