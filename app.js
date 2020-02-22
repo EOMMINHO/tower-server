b72e9e3bde366e7cd52f617c6c30883e6537bb5c
EOMMINHO <djaalsgh159@kaist.ac.kr>
2020-02-22 17:52:01 +0900

    cors error

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var debug = require("debug")("towerServer:server");
+var cors = require("cors");
 
 // Plug and Play
 var pnp = require("./PnP");
@@ -43,6 +44,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser());
 app.use(express.static("public"));
+app.use(cors());
 
 //set routers
 app.use("/", indexRouter);
--- routes/users.js
+++ routes/users.js
@@ -50,16 +50,7 @@ router.post("/signIn", async function(req, res) {
     process.env.JWT_PRIVATE_KEY
   );
   //res.header("x-auth-token", token).send(token);
-  res
-    .set({
-      "x-auth-token": token,
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-      "Access-Control-Max-Age": 3600,
-      "Access-Control-Allow-Headers":
-        "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization"
-    })
-    .send(token);
+  res.send(token);
 });
 
 /*
